20ced5f8133a4cc03155284f2a0d0c63f398d7e7
blaqsheep <odwa@projectcodex.co>
2015-07-29 10:19:39 +0200

    fixed issues with repo and created table creation script and insert scripts for categories and products tables

---
+++ categories_script.sql
@@ -0,0 +1,15 @@
+/*insert script to populate Categories table */
+
+INSERT INTO Categories (name) VALUES ("Bakery");
+INSERT INTO Categories (name) VALUES ("Bulk");
+INSERT INTO Categories (name) VALUES ("Canned Food");
+INSERT INTO Categories (name) VALUES ("Cold Beverages");
+INSERT INTO Categories (name) VALUES ("Confectionaries");
+INSERT INTO Categories (name) VALUES ("Cosmetics");
+INSERT INTO Categories (name) VALUES ("Dairy");
+INSERT INTO Categories (name) VALUES ("Fruits");
+INSERT INTO Categories (name) VALUES ("Soup");
+INSERT INTO Categories (name) VALUES ("Valentine Goodies");
+
+
+
---
+++ nelisa_script.sql
@@ -0,0 +1,38 @@
+
+drop table if exists Purchases;
+drop table if exists Sales;
+drop table if exists Suppliers;
+drop table if exists Products;
+drop table if exists Categories;
+
+create table  Categories (id int not null primary key auto_increment,
+							name varchar(35)
+							);
+
+create table  Products (id int not null primary key auto_increment,
+							name varchar(35),
+							category_id int not null,
+							foreign key (category_id) references Categories(id)
+							);
+
+create table  Suppliers (id int not null primary key auto_increment,
+							name varchar(35)
+							);
+
+
+create table  Sales (id int not null primary key auto_increment, 
+							date date, 
+							product_id int, 
+							no_sold int, 
+							selling_price decimal(10,2),
+							foreign key (product_id) references Products(id)
+								);
+
+create table  Purchases (id int not null primary key auto_increment,
+							product_id int,
+							quantity int,
+							supplier_id int,
+							cost_price decimal(10,2),
+							foreign key (product_id) references Products(id),
+							foreign key (supplier_id) references Suppliers(id)
+							);

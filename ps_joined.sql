ea1b1201c9744c62d5ff00a8f8bc2d91ee8af3c9
blaqsheep <odwa@projectcodex.co>
2015-08-13 09:35:41 +0200

    purchases are displaying on the browser

--- ps_joined.sql
+++ ps_joined.sql
@@ -1,4 +1,4 @@
 
-select Products.name as ProductName, Suppliers.name as SupplierName from stock_purchases_csv 
+select Products.name as ProductName, Suppliers.name as SupplierName, stock_purchases_csv.quantity as Quantity , stock_purchases_csv.total_cost as Total_Cost from stock_purchases_csv 
 	inner join Products on stock_purchases_csv.item = Products.name
 	inner join Suppliers on Suppliers.name = stock_purchases_csv.shop;
--- routes/db_purchases.js
+++ routes/db_purchases.js
@@ -2,9 +2,10 @@ exports.show = function (req, res, next) {
 	req.getConnection(function(err, connection){
 		if (err) 
 			return next(err);
-		connection.query('SELECT * from Suppliers', [], function(err, results) {
+		var query = "select Products.name as ProductName, Suppliers.name as SupplierName, stock_purchases_csv.quantity as Quantity , stock_purchases_csv.total_cost as Total_Cost from stock_purchases_csv  inner join Products on stock_purchases_csv.item = Products.name inner join Suppliers on Suppliers.name = stock_purchases_csv.shop";
+		connection.query(query, [], function(err, results) {
         	if (err) return next(err);
-    		res.render( 'db_suppliers', {
+    		res.render( 'db_purchases', {
     			products : results
     		});
       });
